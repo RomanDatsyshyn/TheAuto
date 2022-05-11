62a8c68852290ef962784c099db1f7b8755e9f1c
Roman Datsyshyn <roman.datsyshyn@hotel-spider.com>
2022-05-11 17:36:09 +0300

    .

--- nextjs/pages/article/[id].js
+++ nextjs/pages/article/[id].js
@@ -1,12 +1,52 @@
-import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import Link from "next/link";
 import { MainLayout } from "../../components/MainLayout";
+import { useRouter } from "next/router";
 
-export default function Article() {
+export default function Article({ article: serverArticle }) {
+  const [article, setArticle] = useState(serverArticle);
   const router = useRouter();
+
+  useEffect(() => {
+    async function load() {
+      const response = await fetch(
+        `http://localhost:1337/api/articles/${router.query.id}`
+      );
+      const data = await response.json();
+      setArticle(data);
+    }
+
+    if (!serverArticle) load();
+  }, []);
+
+  if (!article) {
+    return (
+      <MainLayout>
+        <a>Loading ...</a>
+      </MainLayout>
+    );
+  }
 
   return (
     <MainLayout>
-      <h1>Article {router.query.id}</h1>
+      <Link href={"/"}>
+        <a>Back to Home</a>
+      </Link>
+      <h1>{article.data.attributes.title}</h1>
+      <div>{article.data.attributes.content}</div>
     </MainLayout>
   );
 }
+
+Article.getInitialProps = async ({ query, req }) => {
+  if (!req) {
+    return { article: null };
+  }
+
+  const response = await fetch(
+    `http://localhost:1337/api/articles/${query.id}`
+  );
+  const article = await response.json();
+
+  return { article };
+};
--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -1,7 +1,30 @@
 import Link from "next/link";
+import { useState, useEffect } from "react";
 import { MainLayout } from "../components/MainLayout";
+import { useRouter } from "next/router";
 
-export default function Home() {
+export default function Home({ articles: serverArticles }) {
+  const [articles, setArticles] = useState(serverArticles);
+  const router = useRouter();
+
+  useEffect(() => {
+    async function load() {
+      const response = await fetch(`http://localhost:1337/api/articles/`);
+      const data = await response.json();
+      setArticles(data);
+    }
+
+    if (!serverArticles) load();
+  }, []);
+
+  if (!articles) {
+    return (
+      <MainLayout>
+        <a>Loading ...</a>
+      </MainLayout>
+    );
+  }
+
   return (
     <>
       <MainLayout title={"Home"}>
@@ -16,7 +39,29 @@ export default function Home() {
             <a>Article</a>
           </Link>
         </p>
+        <div>
+          {articles.data.map((i, key) => {
+            return (
+              <Link href={"/article/[id]"} as={`/article/${i.id}`}>
+                <a>
+                  <h3 key={key}>{i.attributes.title}</h3>
+                </a>
+              </Link>
+            );
+          })}
+        </div>
       </MainLayout>
     </>
   );
 }
+
+Home.getInitialProps = async ({ req }) => {
+  if (!req) {
+    return { articles: null };
+  }
+
+  const response = await fetch("http://localhost:1337/api/articles");
+  const articles = await response.json();
+
+  return { articles };
+};
